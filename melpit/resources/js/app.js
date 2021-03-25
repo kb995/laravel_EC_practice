aad3a84dde9af30f72e555eb455a8342cbd1d8f3
kb995 <t.kuboshima995@gmail.com>
2021-03-25 09:02:06 +0900

    section5

--- melpit/resources/js/app.js
+++ melpit/resources/js/app.js
@@ -1,5 +1,15 @@
 require('./bootstrap');
 
+// fontawesome 読み込み (指定したアイコンのみ)
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faAddressCard, faClock } from '@fortawesome/free-regular-svg-icons'
+import { faSearch, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faCamera } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch, faAddressCard, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faClock, faCamera);
+
+dom.watch();
+
+// 画像処理
 document.querySelector('.image-picker input')
 .addEventListener('change', (e) => {
     const input = e.target;
